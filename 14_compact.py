6bd16368fd7a403a311067dadc2992498169e2d6
Alex Linden <alextl2004@gmail.com>
2022-10-14 12:22:42 -0700

    14 & 15

--- 14_compact.py
+++ 14_compact.py
@@ -4,3 +4,14 @@ def compact(lst):
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+
+    out = []
+
+    for el in lst:
+        if el:
+            out.append(el)
+
+    return out
+
+    # with list comprehension
+    # return [val for val in lst if val]
