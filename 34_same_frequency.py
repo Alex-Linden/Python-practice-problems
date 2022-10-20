f8ba44e1e3ac5b1db3799d89b75673ba2eb6d3aa
Alex Linden <alextl2004@gmail.com>
2022-10-20 13:35:39 -0700

    34, 35, 36

--- 34_same_frequency.py
+++ 34_same_frequency.py
@@ -1,12 +1,25 @@
 def same_frequency(num1, num2):
     """Do these nums have same frequencies of digits?
-    
+
         >>> same_frequency(551122, 221515)
         True
-        
+
         >>> same_frequency(321142, 3212215)
         False
-        
+
         >>> same_frequency(1212, 2211)
         True
     """
+
+    return freq_count(num1) == freq_count(num2)
+
+
+def freq_count(nums):
+    counter = {}
+
+    nums = str(nums)
+
+    for num in nums:
+        counter[num] = counter.get(num, 0) + 1
+
+    return counter
--- 36_find_the_duplicate.py
+++ 36_find_the_duplicate.py
@@ -13,3 +13,11 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+
+    visited = set()
+
+    for num in nums:
+        if num in visited:
+            return num
+        visited.add(num)
+
