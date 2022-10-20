42ee760413062594dc2b02bc79af2833bea2e213
Alex Linden <alextl2004@gmail.com>
2022-10-20 14:40:09 -0700

    finished

--- fs_02_valid_parentheses.py
+++ fs_02_valid_parentheses.py
@@ -19,3 +19,16 @@ def valid_parentheses(parens):
         >>> valid_parentheses("((())")
         False
     """
+
+    visited = []
+
+    for p in parens:
+        if p == "(":
+            visited.append(p)
+        else:
+            if len(visited) == 0:
+                return False
+            else:
+                visited.pop()
+
+    return len(visited) == 0
